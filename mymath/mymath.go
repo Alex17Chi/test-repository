504977cdab8a2012738d77105f29acc4ab7923ae
Alex_Chi <nifelgluk@yandex.ru>
2024-07-01 21:05:41 +0500

    added functions Abs, Yn

--- mymath/mymath.go
+++ mymath/mymath.go
@@ -4,6 +4,8 @@ import (
 	"math"
 )
 
+func Abs(x float64) float64 { return math.Abs(x) }
+
 func Sqrt(x float64) float64 { return math.Sqrt(x) }
 
 func Ceil(x float64) float64 { return math.Ceil(x) }
@@ -15,3 +17,5 @@ func Pow(x, y float64) float64 { return math.Pow(x, y) }
 func Max(x, y float64) float64 { return math.Max(x, y) }
 
 func Min(x, y float64) float64 { return math.Min(x, y) }
+
+func Yn(n int, x float64) float64 { return math.Yn(n, x) }
